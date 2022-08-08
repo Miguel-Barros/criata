119f9e5fe3a7a7bca7e4d47624ba399d2667f258
DESKTOP-V0H394L\User <e107ti@etec.sp;gov;br>
2022-08-08 20:07:20 -0300

    Melhor do mundo

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,7 @@ import styles from '../styles/Index.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>Hello world</h1>
+      <h1>Melhor do mundo, receba</h1>
     </div>
   )
 }
