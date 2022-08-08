9ed1a969c1c3a950a7467baff4bea35349c30ffc
DESKTOP-V0H394L\User <e107ti@etec.sp;gov;br>
2022-08-08 20:06:24 -0300

    Initial project

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,7 @@ import styles from '../styles/Index.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>Hi, hello world</h1>
+      <h1>Hello world</h1>
     </div>
   )
 }
