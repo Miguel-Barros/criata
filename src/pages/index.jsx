f03b877aabd61331b638ff20f08c548612cb8bc8
DESKTOP-V0H394L\User <e107ti@etec.sp;gov;br>
2022-08-08 20:41:08 -0300

    Update home page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/Index.module.css'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -10,15 +12,23 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.left}>
           <span className={styles.navigation}>
-            <a href=""><img src='assets/images/index/elipse.png' className={styles.elipse} /></a>
-            <a href=""><img src='assets/images/index/elipse.png' className={styles.elipse} /></a>
-            <a href=""><img src='assets/images/index/elipse.png' className={styles.elipse} /></a>
+            <Link href="/">
+              <img src='/assets/images/index/elipse.png' className={styles.elipse} />
+            </Link>
+            <Link href="/">
+              <img src='/assets/images/index/elipse.png' className={styles.elipse} />
+            </Link>
+            <Link href="/">
+              <img src='/assets/images/index/elipse.png' className={styles.elipse} />
+            </Link>
           </span>
           <div className={styles.box}>
             <h1 className={styles.title}>Bem vindo ao </h1>
             <h1 className={styles.logo}><b className={styles.b}>CRIATA</b></h1>
             <p className={styles.subtitle}>Somoss uma plataforma para criação e personalização de currículos e portifólios online. Faça o seu próprio design e crie uma página exclusiva para você, sem se preocupar com  a programação!</p>
-            <a href="/signIn"><button className={styles.btn}>Vamos começar</button></a>
+            <Link href="/signIn">
+              <button className={styles.btn}>Vamos começar</button>
+            </Link>
           </div>
         </div>
         <div className={styles.right}>
