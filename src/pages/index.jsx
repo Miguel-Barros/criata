4a1ef96238fedd70d608861bb80e2a3873f929c6
DESKTOP-V0H394L\User <e107ti@etec.sp;gov;br>
2022-08-08 20:03:17 -0300

    Zero project

---
+++ src/pages/index.jsx
@@ -0,0 +1,9 @@
+import styles from '../styles/Index.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h1>Helo world</h1>
+    </div>
+  )
+}
