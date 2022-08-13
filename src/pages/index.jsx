4a71440fa76baf9e8942d42d71406a411be7bc5b
David <david.mal04@hotmail.com>
2022-08-13 19:29:11 -0300

    fontes corrigidas e adição de animação nos botões

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -23,7 +23,7 @@ export default function Index() {
           </span>
           <div className={styles.box}>
             <h1 className={styles.title}>Bem vindo ao </h1>
-            <h1 className={styles.logo}><b className={styles.b}>CRIATA</b></h1>
+            <h1 className={styles.logo}><b className={styles.b}>CRIA<span className={styles.b_TA}>TA</span></b></h1>
             <p className={styles.subtitle}>Somos uma plataforma para criação e personalização de currículos e portifólios online. Faça o seu próprio design e crie uma página exclusiva para você, sem se preocupar com  a programação!</p>
             <Link href="/signIn">
               <button className={styles.btn}>Vamos começar</button>
