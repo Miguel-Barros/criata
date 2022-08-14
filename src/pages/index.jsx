760a2c1d5fc5bfbe4e265ffd882252e1e20d0d87
David <david.mal04@hotmail.com>
2022-08-13 21:25:20 -0300

    adicionado página de suporte

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -33,7 +33,7 @@ export default function Index() {
         <div className={styles.right}>
           <img src='assets/images/index/bg-index.svg' className={styles.background} />
           <img src='assets/images/index/ilus-index.svg' className={styles.ilus} />
-          <Link href="#"> 
+          <Link href="/suporte"> 
             <button type="button" className={styles.btn_suporte}>
               <svg xmlns="http://www.w3.org/2000/svg" width="2.5vw" height="2.5vw" fill="currentColor" viewBox="0 0 16 16">
                 <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
