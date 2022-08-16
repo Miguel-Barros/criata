6c86667ff62741ac671d02462556892d47223ff7
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-08-16 17:49:42 -0300

    Config logout

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,15 @@
 import { withProtected } from "../hook/route";;
+import styles from '../styles/Home.module.css';
 
 function Home({ auth }) {
-    const { logout } = auth;
+    const { logout, user } = auth;
     return (
-        <div className="container">
-            <p>Home page</p>
-            <button onClick={logout}>LogOut</button>
+        <div className={styles.container}>
+            <main className={styles.main}>
+                <p className={styles.p}><b className={styles.bold}>Crie o seu próprio design</b><br /> e use um dos templetes de exemplo para inspiração!</p>
+                <button className={styles.btn} onClick={logout}>LogOut</button>
+                <p className={styles.info}>Você esta logado pelo email: {user.email}</p>
+            </main>
         </div>
     )
 }
