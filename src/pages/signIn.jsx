bb63121e6d1354933bd8127f6d83e6eb0743f3f3
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-08-16 18:36:27 -0300

    Modify login page

--- src/pages/signIn.jsx
+++ src/pages/signIn.jsx
@@ -4,7 +4,7 @@ import styles from '../styles/SignIn.module.css';
 import { withPublic } from '../hook/route';
 import Support from '../component/support';
 
- function SignIn({auth}) {
+function SignIn({ auth }) {
     const { loginWithGoogle } = auth;
     return (
         <div className={styles.container}>
@@ -12,20 +12,25 @@ import Support from '../component/support';
                 <title>SignIn - Criata</title>
             </Head>
             <main className={styles.main}>
-                <Support/>
+                <Support />
                 <div className={styles.left}>
                     <h1 className={styles.title}>Login</h1>
                     <input type="text" placeholder='E-mail' className={styles.input} />
                     <input type="password" placeholder='Senha' className={styles.input} />
-                    <button className={styles.btn} onClick={loginWithGoogle}>Login</button>
+                    <button className={styles.btn}>Login</button>
                     <Link href='/signUp'>
                         <a className={styles.link}>Criar conta</a>
                     </Link>
+                    <span className={styles.navlogs}>
+                        <img onClick={loginWithGoogle} src="./assets/images/login/google.svg" />
+                        <img src="./assets/images/login/github.svg" />
+                        <img src="./assets/images/login/microsoft.svg" />
+                    </span>
                     <p className={styles.copyright}>Coaraci © 2022</p>
                 </div>
                 <div className={styles.right}>
-                    <img src="/assets/images/login/bg-ilus.svg" className={styles.background}/>
-                    <img src="/assets/images/login/ilus.svg" className={styles.ilus}/>
+                    <img src="/assets/images/login/bg-ilus.svg" className={styles.background} />
+                    <img src="/assets/images/login/ilus.svg" className={styles.ilus} />
                 </div>
             </main >
         </div >
