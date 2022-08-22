cbf2f686324b19ab6cd3515001f0a8752e1e6247
DESKTOP-V0H394L\User <e107ti@etec.sp;gov;br>
2022-08-22 20:37:08 -0300

    Update search animation

--- src/component/nav.jsx
+++ src/component/nav.jsx
@@ -1,22 +1,40 @@
 import styles from './styles/Nav.module.css'
 
 export default function Nav() {
+    function search() {
+        const i = document.getElementById('input')
+        const search = document.getElementById('search')
+        if (!i) {
+            search.style.width = '250px'
+            search.innerHTML += `
+                <input className={styles.text} id='input' placeholder="Buscar" type="text" />
+            `
+        }else{
+            search.style.width = '45px'
+            search.innerHTML = `
+                <img className={styles.search_i} src="./assets/components/search.svg" alt="" />
+            `
+        }
+    }
+
     return (
         <>
             <nav className={styles.nav}>
                 <ul className={styles.ul}>
                     <li>Item</li>
-                </ul> 
+                </ul>
                 <ul className={styles.ul}>
                     <img className={styles.s_icon} src="./assets/components/settings-icon.svg" />
                     <p className={styles.p_name}>Wanda Django</p>
                     <img className={styles.profile} src="./assets/components/profile.svg" />
                 </ul>
-                <span className={styles.search}>
+                <span className={styles.search} id={'search'} onClick={search}>
                     <img className={styles.search_i} src="./assets/components/search.svg" alt="" />
-                    <input className={styles.text} placeholder="Buscar" type="text" />
                 </span>
             </nav>
         </>
     )
 }
+
+{/* <input className={styles.text} id='input' placeholder="Buscar" type="text" /> */ }
+
