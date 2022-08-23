82134d64623128d19c975cc509489fd145516d17
DESKTOP-V0H394L\User <e107ti@etec.sp;gov;br>
2022-08-23 14:07:06 -0300

    Modify home page

--- src/component/nav.jsx
+++ src/component/nav.jsx
@@ -1,18 +1,14 @@
 import styles from './styles/Nav.module.css'
 
 export default function Nav() {
-    function search() {
-        const i = document.getElementById('input')
-        const search = document.getElementById('search')
-        if (!i) {
-            search.style.width = '250px'
-            search.innerHTML += `
-                <input className={styles.text} id='input' placeholder="Buscar" type="text" />
-            `
-        }else{
-            search.style.width = '45px'
-            search.innerHTML = `
-                <img className={styles.search_i} src="./assets/components/search.svg" alt="" />
+
+    function Search() {
+        const input = document.getElementById('input')
+        const box = document.getElementById('search')
+        if (!input) {
+            box.style.width = '250px'
+            box.innerHTML += `
+                <input className={styles.input} id='input' placeholder="Buscar usuario" type="text" />
             `
         }
     }
@@ -20,21 +16,10 @@ export default function Nav() {
     return (
         <>
             <nav className={styles.nav}>
-                <ul className={styles.ul}>
-                    <li>Item</li>
-                </ul>
-                <ul className={styles.ul}>
-                    <img className={styles.s_icon} src="./assets/components/settings-icon.svg" />
-                    <p className={styles.p_name}>Wanda Django</p>
-                    <img className={styles.profile} src="./assets/components/profile.svg" />
-                </ul>
-                <span className={styles.search} id={'search'} onClick={search}>
+                <span className={styles.search} id={'search'} onClick={Search}>
                     <img className={styles.search_i} src="./assets/components/search.svg" alt="" />
                 </span>
             </nav>
         </>
     )
 }
-
-{/* <input className={styles.text} id='input' placeholder="Buscar" type="text" /> */ }
-
