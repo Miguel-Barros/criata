ac873b5730765e626d86db94ab024ddcf7d8e797
DESKTOP-V0H394L\User <e107ti@etec.sp;gov;br>
2022-08-23 16:34:56 -0300

    Modify home page²

--- src/component/nav.jsx
+++ src/component/nav.jsx
@@ -1,7 +1,6 @@
 import styles from './styles/Nav.module.css'
 
-export default function Nav() {
-
+export default function Nav(props) {
     function Search() {
         const input = document.getElementById('input')
         const box = document.getElementById('search')
@@ -16,8 +15,21 @@ export default function Nav() {
     return (
         <>
             <nav className={styles.nav}>
-                <span className={styles.search} id={'search'} onClick={Search}>
-                    <img className={styles.search_i} src="./assets/components/search.svg" alt="" />
+                <span className={styles.box}>
+                    <div className={styles.logo}>
+                        <img className={styles.l_icon} src="./assets/components/criata_logo.svg" alt="" />
+                        <p className={styles.l_text}>Criata</p>
+                    </div>
+                    <div className={styles.profile}>
+                        <img className={styles.s_icon} src="./assets/components/settings-icon.svg" />
+                        <p className={styles.p_name}>{props.displayName}</p>
+                        <img className={styles.p_icon} src="./assets/components/profile.png" alt="" />
+                    </div>
+                </span>
+                <span className={styles.search}>
+                    <span className={styles.s_box} id={'search'} onClick={Search}>
+                        <img className={styles.s_icon} src="./assets/components/search.svg" alt="" />
+                    </span>
                 </span>
             </nav>
         </>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,11 +4,14 @@ import Head from "next/head";
 import Nav from "../component/nav";
 
 function Home({ auth }) {
+
+    const { user } = auth
+
     return (
         <div className={styles.container}>
             <Head><title>Criata - Home</title></Head>
             <main className={styles.main}>
-                <Nav />
+                <Nav displayName={user.displayName}/>
                 <img src="./assets/images/home/bg.svg" className={styles.bg} />
                 <p className={styles.p}><b className={styles.bold}>Crie o seu próprio design</b><br /> e use um dos templetes de exemplo para inspiração!</p>
             </main>
