9b4964d51b866ec58e7a1961661f2c68f5a5052d
DESKTOP-V0H394L\User <e107ti@etec.sp;gov;br>
2022-09-08 18:48:28 -0300

    Finish about page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,6 +1,7 @@
 import styles from '../styles/About.module.css'
 import SideNav from '../component/side-nav'
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default function About() {
     return (
@@ -8,20 +9,24 @@ export default function About() {
             <Head>
                 <title>Criata - Sobre nós</title>
             </Head>
-            <div className={styles.main}>
-                <SideNav />
-                <img className={styles.bg} src="./assets/images/about/bg.svg" alt="background" />
-                <span className={styles.logo}>
-                    <img className={styles.team_logo} src="./assets/images/about/team-logo.svg" alt="team-logo" />
-                    <h3 className={styles.team_name} >COARACI</h3>
-                </span>
-                <div className={styles.box}>
-                    <h1 className={styles.title}>Equipe <span className={styles.bold}>Coaraci</span></h1>
-                    <p className={styles.text}>Somos uma equipe de 5 (cinco) integrantes, trazendo suas brasilidades e as cores, tendo como meta, possibilitar que qualquer pessoa tenha seu trabalho profissional organizado, de forma única e referente a sua identidade sem perder a praticidade.</p>
-                    <p className={styles.text}>Nos conheça mais a seguir! </p>
-                    <img className={styles.arrow} src="./assets/components/arrow.svg" alt="arrow" />
+            <section className={`${styles.intro} ${styles.section}`}>
+                <div className={styles.main}>
+                    <SideNav />
+                    <img className={styles.bg} src="./assets/images/about/bg.svg" alt="background" />
+                    <span className={styles.logo}>
+                        <img className={styles.team_logo} src="./assets/images/about/team-logo.svg" alt="team-logo" />
+                        <h3 className={styles.team_name} >COARACI</h3>
+                    </span>
+                    <div className={styles.box}>
+                        <h1 className={styles.title}>Equipe <span className={styles.bold}>Coaraci</span></h1>
+                        <p className={styles.text}>Somos uma equipe de 5 (cinco) integrantes, trazendo suas brasilidades e as cores, tendo como meta, possibilitar que qualquer pessoa tenha seu trabalho profissional organizado, de forma única e referente a sua identidade sem perder a praticidade.</p>
+                        <p className={styles.text}>Nos conheça mais a seguir! </p>
+                        <Link href='/team'>
+                            <img className={styles.arrow} src="./assets/components/arrow.svg" alt="arrow" />
+                        </Link>
+                    </div>
                 </div>
-            </div>
+            </section>
         </div>
     )
 }
---
+++ src/pages/team.jsx
@@ -0,0 +1,65 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import styles from '../styles/Team.module.css'
+
+export default function Team() {
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Criata - Nossa equipe</title>
+            </Head>
+            <span className={styles.mini_box}>
+                <Link href={'/about'}>
+                    <img className={styles.arrow} src="./assets/components/arrow.svg" alt="arrow" />
+                </Link>
+                <h1 className={styles.title}>Conheça a nossa <span className={styles.bold}>equipe</span></h1>
+            </span>
+            <div className={styles.main}>
+                <div className={styles.card_group}>
+                    <div className={styles.card}>
+                        <img className={styles.photo} src="./assets/images/team/bruno.svg" alt="" />
+                        <p className={styles.member_name}>Bruno Salinas</p>
+                        <p className={styles.member_prefix}>@brunosalinas</p>
+                        <div className={styles.bio}>
+                            <p className={styles.bio_text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sit amet cursus tellus, accumsan aliquet turpis. Ut vitae sodales erat. </p>
+                        </div>
+                    </div>
+                    <div className={styles.card}>
+                        <img className={styles.photo} src="./assets/images/team/david.svg" alt="" />
+                        <p className={styles.member_name}>David Larrosa</p>
+                        <p className={styles.member_prefix}>@davidlarrosa</p>
+                        <div className={styles.bio}>
+                            <p className={styles.bio_text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sit amet cursus tellus, accumsan aliquet turpis. Ut vitae sodales erat. </p>
+                        </div>
+                    </div>
+                    <div className={styles.card}>
+                        <img className={styles.photo} src="./assets/images/team/lorena.svg" alt="" />
+                        <p className={styles.member_name}>Lorena Sobral</p>
+                        <p className={styles.member_prefix}>@lorenasobral</p>
+                        <div className={styles.bio}>
+                            <p className={styles.bio_text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sit amet cursus tellus, accumsan aliquet turpis. Ut vitae sodales erat. </p>
+                        </div>
+                    </div>
+                    <div className={styles.card}>
+                        <img className={styles.photo} src="./assets/images/team/maria.svg" alt="" />
+                        <p className={styles.member_name}>Maria Ramos</p>
+                        <p className={styles.member_prefix}>@mariaramos</p>
+                        <div className={styles.bio}>
+                            <p className={styles.bio_text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sit amet cursus tellus, accumsan aliquet turpis. Ut vitae sodales erat. </p>
+                        </div>
+                    </div>
+                    <div className={styles.card}>
+                        <img className={styles.photo} src="./assets/images/team/miguel.svg" alt="" />
+                        <p className={styles.member_name}>Miguel de Barros</p>
+                        <p className={styles.member_prefix}>@miguelbarros</p>
+                        <div className={styles.bio}>
+                            <p className={styles.bio_text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sit amet cursus tellus, accumsan aliquet turpis. Ut vitae sodales erat. </p>
+                        </div>
+                    </div>
+                </div>
+                <img className={styles.ilus_bg} src="./assets/images/team/bg-ilus.svg" alt="background" />
+                <img className={styles.bg} src="./assets/images/team/bg.svg" alt="background" />
+            </div>
+        </div>
+    )
+}
