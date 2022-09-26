5ca25062ca6deebf6db5cf0d8e8d69016efe7bf7
DESKTOP-V0H394L\User <e107ti@etec.sp;gov;br>
2022-09-26 20:43:20 -0300

    Create home page

--- src/component/nav.jsx
+++ src/component/nav.jsx
@@ -6,7 +6,22 @@ export default function Nav(props) {
     return (
         <>
             <header className={styles.header}>
-                <img src="./assets/components/criata_logo.svg" alt="criata-logo" className={styles.logo} />
+                <span className={styles.left}>
+                    <span className={styles.logo}>
+                        <img src="./assets/components/criata_logo.svg" alt="criata-logo" />
+                        <p>Criata</p>
+                    </span>
+                </span>
+                <span className={styles.right}>
+                    <span className={styles.box}>
+                        <Icon icon={'mdi:cog-outline'} className={styles.icons} />
+                        <p>{props.name}</p>
+                        <Icon icon={'mdi:account-circle'} className={styles.profile} />
+                        <span className={styles.split} />
+                        <input type="text" className={styles.search} />
+                        <Icon icon={'mdi:magnify'} className={styles.search_icon} />
+                    </span>
+                </span>
             </header>
         </>
     )
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,6 +2,7 @@ import styles from '../styles/Home.module.css';
 import Head from "next/head";
 import { withProtected } from '../hook/route';
 import Nav from '../component/nav';
+import { Icon } from '@iconify/react';
 
 function Home({ auth }) {
     const { logout, user } = auth
@@ -11,13 +12,42 @@ function Home({ auth }) {
             <Head>
                 <title>Criata - Home</title>
             </Head>
-            <Nav />
+            <Nav name={user.email} />
             <main style={styles.main}>
                 <img src="./assets/images/home/bg.svg" alt="background" className={styles.background} />
                 <span className={styles.welcome}>
                     <h1>Crie o seu próprio design!</h1>
-                    <p>e use um dos h de exemplo para inspiração</p>
+                    <p>Utilize um de nossos <b>templates</b> para inspiração<br />
+                        e <b>diferencie-se</b> das outras pessoas!</p>
                 </span>
+                <div className={styles.card_group}>
+                    <div className={styles.card}>
+                        <Icon icon={'mdi:plus-circle-outline'} className={styles.plus} />
+                    </div>
+                    <div className={styles.card}>
+                        <Icon icon={'mdi:plus-circle-outline'} className={styles.plus} />
+                    </div>
+                    <div className={styles.card}>
+                        <Icon icon={'mdi:plus-circle-outline'} className={styles.plus} />
+                    </div>
+                    <div className={styles.card}>
+                        <Icon icon={'mdi:plus-circle-outline'} className={styles.plus} />
+                    </div>
+                </div>
+                <div className={styles.card_group}>
+                    <div className={styles.card}>
+                        <Icon icon={'mdi:plus-circle-outline'} className={styles.plus} />
+                    </div>
+                    <div className={styles.card}>
+                        <Icon icon={'mdi:plus-circle-outline'} className={styles.plus} />
+                    </div>
+                    <div className={styles.card}>
+                        <Icon icon={'mdi:plus-circle-outline'} className={styles.plus} />
+                    </div>
+                    <div className={styles.card}>
+                        <Icon icon={'mdi:plus-circle-outline'} className={styles.plus} />
+                    </div>
+                </div>
             </main>
         </div>
     )
