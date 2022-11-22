582debbe40e9ae8342a5986886cd7f9f05e413ba
DESKTOP-V0H394L\User <e107ti@etec.sp;gov;br>
2022-11-22 18:47:17 -0300

    !

--- src/component/utils/importElement.jsx
+++ src/component/utils/importElement.jsx
@@ -8,35 +8,29 @@ export default function ImportElement() {
     const [file, setFile] = useState(null);
     const [imports, setImports] = useState([]);
 
-    useEffect(() => {
-        if (file) {
-            imports.push(file);
-        }
-    }, [file]);
-
-    useEffect(() => {
-        document.getElementById("file").onchange = () => {
-            setFile(document.getElementById("file").files[0]);
-        }
-    }, []);
-
     return (
         <>
             <div className={styles.tool_content}>
                 <p>Ao importar uma imagem, ela aparecerá aqui!</p>
                 <div className={styles.tool_content_group} id="tool_content_group" >
-                    <input type="file" id="file" className={styles.file} />
+                    <input type="file" id="file" className={styles.file} onChange={(e) => {
+                        setFile(e.target.files[0])
+                        setImports([...imports, file])
+                    }} />
                     {imports.map((e, i) => {
                         return (
                             <div className={styles.importObject} key={i}>
-                                <img src={URL.createObjectURL(e)} alt="imported-element" />
+                                <img src={URL.createObjectURL(
+                                    (e != null ? e : file)
+                                )} alt="imported-element" />
                             </div>
                         )
                     })}
                     {/* <div className={styles.importObject}></div> */}
                 </div>
-                <button className={styles.btn}
-                    onClick={() => document.querySelector("#file").click()} >Adicionar Arquivos</button>
+                <button className={styles.btn} id="btn" onClick={() =>
+                    document.querySelector("#file").click()
+                }>Adicionar Arquivos</button>
             </div>
         </>
     )
