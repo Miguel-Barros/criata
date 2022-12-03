fbf9f0b76981a795580966b2450f3595d6a96307
DESKTOP-V0H394L\User <e107ti@etec.sp;gov;br>
2022-12-02 22:35:18 -0300

    Pao

--- src/component/qrCode.jsx
+++ src/component/qrCode.jsx
@@ -1,10 +1,34 @@
 import { Icon } from "@iconify/react"
+import { useEffect, useState } from "react"
+import useAuth from "../hook/auth";
 import styles from './styles/QRCode.module.css'
+import Database from "../services/Database";
+import CodeQR from "react-qr-code";
 
 function QRCode({ showing, onClose }) {
     if (!showing) return null
+    const { user } = useAuth()
+    const [userData, setUserData] = useState(null)
+
+    useEffect(() => {
+        Database.getUserData(user.uid).then((data) => {
+            setUserData(data)
+        })
+    }, [])
+
     return (
         <div className={styles.container}>
+            <div className={styles.modal}>
+                <Icon icon="mdi:close" className={styles.close} onClick={onClose} />
+                <h2>Meu codigo QR</h2>
+                <img className={styles.profile} src={userData?.imgProfile} alt="Profile-Img" />
+                <h3>{userData?.fullName}</h3>
+                <h3>{userData?.username}</h3>
+                <span>
+                    <CodeQR size={180} className={styles.qrCode} value={`https://criata.me/profile/${userData.username.replace('@', '')}`} />
+                </span>
+                <button className={styles.btn}>Compartilhar Codigo QR</button>
+            </div>
         </div>
     )
 }
