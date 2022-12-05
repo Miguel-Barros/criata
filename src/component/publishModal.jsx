269541de1ae810e7c15af2d73e757118f1d95997
Miguel-Barros <eomiguelbarros@gmail.com>
2022-12-05 10:41:26 -0300

    Publish

--- src/component/publishModal.jsx
+++ src/component/publishModal.jsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
 import styles from "./styles/PublishModal.module.css";
 
 export default function PublishModal({ showing, onClose }) {
     if (!showing) return null;
+
+    const [defaultPrefix, setDefaultPrefix] = useState("https://criata.me/");
+    const [prefix, setPrefix] = useState("");
 
     return (
         <div className={styles.container}>
@@ -9,10 +13,16 @@ export default function PublishModal({ showing, onClose }) {
                 <h2>Vamos publica-lo na Web!</h2>
                 <img src="./assets/images/creation/ilusPublish.svg" alt="ilustration" />
                 <p>Digite o nome que deseja para seu projeto</p>
-                <input type="text" placeholder="Nome do projeto" value={'https://criata.me/' + 'SeuPrefixo'} />
+                
+                <input type="text" placeholder="https://criata.me/"
+                    value={"https://criata.me/" + prefix}
+                    onChange={(e) => setPrefix(e.target.value.slice(18).replace(' ', ''))}
+                />
                 <span>
                     <button onClick={onClose}>Cancelar</button>
-                    <button onClick={() => { }}>Publicar</button>
+                    <button onClick={() => { 
+                        alert("Publicado com sucesso! em: " + defaultPrefix + prefix);
+                    }}>Publicar</button>
                 </span>
             </div>
         </div>
